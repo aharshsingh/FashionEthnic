ecc07b0547f124c5f8ca4a68249a4d40ffb5b7b5
aharshsingh <aharshsingh25@gmail.com>
2025-02-17 23:35:27 +0530

    size chart

--- frontend/frontend-app/src/component/SizeChart.jsx
+++ frontend/frontend-app/src/component/SizeChart.jsx
@@ -16,12 +16,12 @@ export default function SizeChart() {
       }
   return (
     <div>
-        <div>
-            <img className='crossImg' src={crossImg} onClick = { handleClick } alt="logo"/>
+        <div className='flex justify-end mt-4'>
+            <img className='w-6 h-6' src={crossImg} onClick = { handleClick } alt="logo"/>
         </div>
         {gender ==='male' ?
-            (<img className='sizeChartImageM' src={menSizeChart} alt='sizeChart' />) :
-            (<img className='sizeChartImageW' src={womenSizechart} alt='sizeChart' />)
+            (<div className='flex justify-center items-center '><img className='sizeChartImageM' src={menSizeChart} alt='sizeChart' /></div>) :
+            (<div className='flex justify-center items-center '><img className='sizeChartImageW' src={womenSizechart} alt='sizeChart' /></div>)
         }
     </div>
   )
